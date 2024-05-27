a88bc0183a81a34e4496a13ba903fc34d94110e0
Michael Davenport <mdaven19@byu.edu>
2024-05-27 16:06:29 -0600

    add request documentation

--- src/main/java/edu/byu/minecraft/cat/model/JoinRequest.java
+++ src/main/java/edu/byu/minecraft/cat/model/JoinRequest.java
@@ -2,4 +2,12 @@ package edu.byu.minecraft.cat.model;
 
 import java.util.UUID;
 
+/**
+ * Represents a request of one player to join a civ
+ *
+ * @param ID        unique request ID
+ * @param timestamp when the request was made
+ * @param requester UUID of requesting player
+ * @param civID     civ player is requesting to join
+ */
 public record JoinRequest(int ID, long timestamp, UUID requester, int civID) {}
