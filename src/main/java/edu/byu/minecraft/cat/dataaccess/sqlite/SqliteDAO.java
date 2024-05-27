26f1222f0ecf1b005e4ae4201326f9ebd670073a
Michael Davenport <mdaven19@byu.edu>
2024-05-27 15:17:49 -0600

    add sqlite dao documentation

--- src/main/java/edu/byu/minecraft/cat/dataaccess/sqlite/SqliteDAO.java
+++ src/main/java/edu/byu/minecraft/cat/dataaccess/sqlite/SqliteDAO.java
@@ -12,16 +12,34 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public abstract class SqliteDAO {
+    private static final String SEPARATOR = " ";
+
     private static final File FOLDER = new File(String.format("config/%s", CivsAndTitles.MOD_ID));
 
     private static final String FILE_LOCATION = FOLDER.getPath() + "/database.db";
 
     private static boolean databaseCreated = false;
 
+
+    /**
+     * Constructs a new sqlite DAO. If the database file doesn't already exist it is created
+     *
+     * @throws DataAccessException if the database file could not be created
+     */
     protected SqliteDAO() throws DataAccessException {
         configureDatabase();
     }
 
+
+    /**
+     * Executes a SQL update
+     *
+     * @param statement SQL statement to execute containing an update statement without output. Contains '?'s equal to
+     *                  the length of params
+     * @param params    Array of objects as parameters to the sql statement
+     * @return Generated sql auto increment value, if applicable. 0 otherwise.
+     * @throws DataAccessException if something goes wrong with sql execution
+     */
     protected int executeUpdate(String statement, Object... params) throws DataAccessException {
         return execute(conn -> {
             try (PreparedStatement ps = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
@@ -34,6 +52,18 @@ public abstract class SqliteDAO {
         });
     }
 
+
+    /**
+     * Performs a SQL query.
+     *
+     * @param statement SQL statement to execute containing a query with output. Contains '?'s equal to the length of
+     *                  params
+     * @param parser    What to do with the result set when obtained
+     * @param params    Array of objects as parameters to the sql statement
+     * @param <T>       return type
+     * @return the return value of parser
+     * @throws DataAccessException if something goes wrong with sql execution
+     */
     protected <T> T executeQuery(String statement, ResultSetParser<T> parser, Object... params)
             throws DataAccessException {
         return execute(conn -> {
@@ -46,6 +76,15 @@ public abstract class SqliteDAO {
         });
     }
 
+
+    /**
+     * Executes code for a connection. Synchronized to ensure only one connection exists
+     *
+     * @param function code to execute on the connection
+     * @param <T>      return type of function
+     * @return the return value of function
+     * @throws DataAccessException if something goes wrong with sql execution
+     */
     private synchronized <T> T execute(SQLFunction<T> function) throws DataAccessException {
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:" + FILE_LOCATION)) {
             return function.apply(conn);
@@ -54,6 +93,15 @@ public abstract class SqliteDAO {
         }
     }
 
+
+    /**
+     * Adds parameters to a prepared statement
+     *
+     * @param ps     SQL PreparedStatement containing executable sql with '?'s equal to the length of params
+     * @param params Parameters to fill ps with
+     * @throws SQLException        if ps is closed or there are more params than '?'s in ps
+     * @throws DataAccessException if params contains an unexpected data type
+     */
     private void addParams(PreparedStatement ps, Object[] params) throws SQLException, DataAccessException {
         for (int i = 0; i < params.length; i++) {
             Object param = params[i];
@@ -71,16 +119,36 @@ public abstract class SqliteDAO {
         }
     }
 
+
+    /**
+     * Turns a set of UUID's into a string for use in sql
+     *
+     * @param set a set of UUID's to turn into a string
+     * @return a string containing a SEPARATOR separated list of the UUID's from set
+     */
     protected String setToString(Set<UUID> set) {
         StringBuilder builder = new StringBuilder();
-        for (UUID uuid : set) builder.append(uuid.toString()).append(' ');
+        for (UUID uuid : set) builder.append(uuid.toString()).append(SEPARATOR);
         return builder.toString().trim();
     }
 
+
+    /**
+     * Turns a string generated from setToString back into a set of UUID's
+     *
+     * @param str a string containing a SEPARATOR separated list of UUID's
+     * @return a set of UUID's
+     */
     protected Set<UUID> stringToSet(String str) {
-        return Arrays.stream(str.split(" ")).map(UUID::fromString).collect(Collectors.toCollection(HashSet::new));
+        return Arrays.stream(str.split(SEPARATOR)).map(UUID::fromString).collect(Collectors.toCollection(HashSet::new));
     }
 
+
+    /**
+     * Creates a new database file if it doesn't already exist
+     *
+     * @throws DataAccessException if file creation goes wrong
+     */
     protected void configureDatabase() throws DataAccessException {
         if (!databaseCreated) {
             try {
@@ -99,13 +167,40 @@ public abstract class SqliteDAO {
     }
 
 
+    /**
+     * Parses a result set and returns the result
+     *
+     * @param <T> return type
+     */
     @FunctionalInterface
     protected interface ResultSetParser<T> {
+        /**
+         * Parses a result set
+         *
+         * @param rs ResultSet to parse
+         * @return Contents of ResultSet as java object(s)
+         * @throws SQLException if a database access error occurs, the result set is closed, or something goes wrong
+         *                      with SQL execution
+         */
         T parseResultSet(ResultSet rs) throws SQLException;
     }
 
+
+    /**
+     * Executes sql on a provided connection
+     *
+     * @param <T> return type
+     */
     @FunctionalInterface
     private interface SQLFunction<T> {
+        /**
+         * Executes sql code on a connection
+         *
+         * @param conn the connection to use
+         * @return return value of sql code
+         * @throws SQLException        If SQL syntax is incorrect or other errors in sql execution
+         * @throws DataAccessException If non-sql errors arise
+         */
         T apply(Connection conn) throws SQLException, DataAccessException;
     }
 }
