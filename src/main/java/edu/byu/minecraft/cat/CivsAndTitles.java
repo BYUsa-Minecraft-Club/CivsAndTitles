12fd500a18afaba4e92b0699f8e9bf93459e1422
Michael Davenport <mdaven19@byu.edu>
2024-05-27 14:52:16 -0600

    separate mod id

--- src/main/java/edu/byu/minecraft/cat/CivsAndTitles.java
+++ src/main/java/edu/byu/minecraft/cat/CivsAndTitles.java
@@ -9,10 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CivsAndTitles implements ModInitializer {
+	public static final String MOD_ID = "civsandtitles";
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("civsandtitles");
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	private static DataAccess dataAccess;
 
