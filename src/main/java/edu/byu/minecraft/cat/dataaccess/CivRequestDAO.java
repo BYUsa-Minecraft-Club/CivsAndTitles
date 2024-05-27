71b969417d60f2a9acfb9e613a5e2e40f59dbed0
Michael Davenport <mdaven19@byu.edu>
2024-05-27 15:39:59 -0600

    add request dao documentation

--- src/main/java/edu/byu/minecraft/cat/dataaccess/CivRequestDAO.java
+++ src/main/java/edu/byu/minecraft/cat/dataaccess/CivRequestDAO.java
@@ -2,6 +2,9 @@ package edu.byu.minecraft.cat.dataaccess;
 
 import edu.byu.minecraft.cat.model.CivRequest;
 
+/**
+ * Data Access interface for civ creation requests
+ */
 public interface CivRequestDAO extends SimpleDAO<CivRequest, Integer> {
 
 }
