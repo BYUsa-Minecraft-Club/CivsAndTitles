6c6c66f0c3d8b55ef86053ec1ddc1a6828f4826f
Michael Davenport <mdaven19@byu.edu>
2024-05-27 16:11:03 -0600

    add location documentation

--- src/main/java/edu/byu/minecraft/cat/model/Location.java
+++ src/main/java/edu/byu/minecraft/cat/model/Location.java
@@ -2,5 +2,14 @@ package edu.byu.minecraft.cat.model;
 
 import net.minecraft.util.Identifier;
 
-public record Location(int x, int y, int z, Identifier world, float yaw, float pitch) {
-}
+/**
+ * Represents a location in minecraft a player can teleport to
+ *
+ * @param x     x block coordinate (+East/-West)
+ * @param y     y block coordinate (+Up/-Down)
+ * @param z     z block coordinate (+South/-North)
+ * @param world identifier of world location is in
+ * @param yaw   direction (horizontal) a player is facing
+ * @param pitch direction (vertical) a player is facing
+ */
+public record Location(int x, int y, int z, Identifier world, float yaw, float pitch) {}
