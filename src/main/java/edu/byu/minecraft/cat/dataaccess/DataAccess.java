37880006b1aebbbb4fe2c545a409e076e7c6e757
Michael Davenport <mdaven19@byu.edu>
2024-05-27 15:53:12 -0600

    add data access documentation

--- src/main/java/edu/byu/minecraft/cat/dataaccess/DataAccess.java
+++ src/main/java/edu/byu/minecraft/cat/dataaccess/DataAccess.java
@@ -1,12 +1,71 @@
 package edu.byu.minecraft.cat.dataaccess;
 
+/**
+ * Abstract factory interface for DAO's of a particular storage system
+ */
 public interface DataAccess {
+
+    /**
+     * Gets/Makes a civ DAO for the current storage system
+     * @return a civ DAO
+     * @throws DataAccessException If database cannot be accessed
+     */
     CivDAO getCivDAO() throws DataAccessException;
+
+
+    /**
+     * Gets/Makes a build DAO for the current storage system
+     * @return a build DAO
+     * @throws DataAccessException If database cannot be accessed
+     */
     BuildDAO getBuildDAO() throws DataAccessException;
+
+
+    /**
+     * Gets/Makes a build score DAO for the current storage system
+     * @return a build score DAO
+     * @throws DataAccessException If database cannot be accessed
+     */
     BuildScoreDAO getBuildScoreDAO() throws DataAccessException;
+
+
+    /**
+     * Gets/Makes a player DAO for the current storage system
+     * @return a player DAO
+     * @throws DataAccessException If database cannot be accessed
+     */
     PlayerDAO getPlayerDAO() throws DataAccessException;
+
+
+    /**
+     * Gets/Makes a civ request DAO for the current storage system
+     * @return a civ request DAO
+     * @throws DataAccessException If database cannot be accessed
+     */
     CivRequestDAO getCivRequestDAO() throws DataAccessException;
+
+
+    /**
+     * Gets/Makes a join request DAO for the current storage system
+     * @return a join request DAO
+     * @throws DataAccessException If database cannot be accessed
+     */
     JoinRequestDAO getJoinRequestDAO() throws DataAccessException;
+
+
+    /**
+     * Gets/Makes a title DAO for the current storage system
+     * @return a title DAO
+     * @throws DataAccessException If database cannot be accessed
+     */
     TitleDAO getTitleDAO() throws DataAccessException;
+
+
+    /**
+     * Gets/Makes an unlocked title DAO for the current storage system
+     * @return an unlocked title DAO
+     * @throws DataAccessException If database cannot be accessed
+     */
     UnlockedTitleDAO getUnlockedTitleDAO() throws DataAccessException;
+
 }
--- src/main/java/edu/byu/minecraft/cat/dataaccess/DataAccessException.java
+++ src/main/java/edu/byu/minecraft/cat/dataaccess/DataAccessException.java
@@ -1,15 +1,38 @@
 package edu.byu.minecraft.cat.dataaccess;
 
-public class DataAccessException extends Exception{
+/**
+ * An exception that provides information on a database access errors or other related errors.
+ */
+public class DataAccessException extends Exception {
+
+    /**
+     * Constructs a new DataAccessException with a given message
+     *
+     * @param message a description of the exception
+     */
     public DataAccessException(String message) {
         super(message);
     }
 
+
+    /**
+     * Constructs a new DataAccessException with a given message and cause
+     *
+     * @param message a description of the exception
+     * @param cause   an underlying cause of this exception
+     */
     public DataAccessException(String message, Throwable cause) {
         super(message, cause);
     }
 
+
+    /**
+     * Constructs a new DataAccessException with a given cause
+     *
+     * @param cause an underlying cause of this exception
+     */
     public DataAccessException(Throwable cause) {
         super(cause);
     }
+
 }
