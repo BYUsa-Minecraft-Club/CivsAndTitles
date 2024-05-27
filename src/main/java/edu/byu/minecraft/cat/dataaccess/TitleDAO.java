17c5c4d8c73f0c6849d4a72218fd11f8a300fc2b
Michael Davenport <mdaven19@byu.edu>
2024-05-27 15:47:18 -0600

    add title daos documentation

--- src/main/java/edu/byu/minecraft/cat/dataaccess/TitleDAO.java
+++ src/main/java/edu/byu/minecraft/cat/dataaccess/TitleDAO.java
@@ -2,6 +2,9 @@ package edu.byu.minecraft.cat.dataaccess;
 
 import edu.byu.minecraft.cat.model.Title;
 
+/**
+ * Data Access interface for titles
+ */
 public interface TitleDAO extends SimpleDAO<Title, String> {
 
 }
--- src/main/java/edu/byu/minecraft/cat/dataaccess/UnlockedTitleDAO.java
+++ src/main/java/edu/byu/minecraft/cat/dataaccess/UnlockedTitleDAO.java
@@ -5,11 +5,56 @@ import edu.byu.minecraft.cat.model.UnlockedTitle;
 import java.util.Collection;
 import java.util.UUID;
 
+/**
+ * Data Access interface for titles unlocked by players
+ */
 public interface UnlockedTitleDAO {
+
+    /**
+     * Gets all titles unlocked by a player
+     *
+     * @param uuid uuid of player
+     * @return all titles unlocked by player
+     * @throws DataAccessException If database cannot be accessed
+     */
     Collection<UnlockedTitle> getAll(UUID uuid) throws DataAccessException;
+
+
+    /**
+     * Gets all titles matching a title
+     *
+     * @param title title to match
+     * @return all unlocked titles matching title
+     * @throws DataAccessException If database cannot be accessed
+     */
     Collection<UnlockedTitle> getAll(String title) throws DataAccessException;
-    int insert(UnlockedTitle title) throws DataAccessException;
+
+
+    /**
+     * Adds a new unlocked title
+     *
+     * @param title unlocked title to add
+     * @throws DataAccessException If database cannot be accessed
+     */
+    void insert(UnlockedTitle title) throws DataAccessException;
+
+
+    /**
+     * Deletes a particular unlocked title
+     *
+     * @param uuid  player to remove title from
+     * @param title title to remove
+     * @throws DataAccessException If database cannot be accessed
+     */
     void delete(UUID uuid, String title) throws DataAccessException;
+
+
+    /**
+     * Deletes all unlocked titles matching a title
+     *
+     * @param title title to match
+     * @throws DataAccessException If database cannot be accessed
+     */
     void deleteAll(String title) throws DataAccessException;
-    void update(UnlockedTitle title) throws DataAccessException;
+
 }
