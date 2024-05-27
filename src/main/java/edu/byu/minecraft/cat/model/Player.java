038d9f0d661ac7b930c13cfebb4b281397048785
Michael Davenport <mdaven19@byu.edu>
2024-05-27 16:14:24 -0600

    add player documentation

--- src/main/java/edu/byu/minecraft/cat/model/Player.java
+++ src/main/java/edu/byu/minecraft/cat/model/Player.java
@@ -2,8 +2,40 @@ package edu.byu.minecraft.cat.model;
 
 import java.util.UUID;
 
+/**
+ * Represents a player
+ *
+ * @param uuid   UUID of player profile
+ * @param name   current player name
+ * @param points personal build points
+ * @param title  current title
+ * @param role   player role
+ */
 public record Player(UUID uuid, String name, int points, String title, Role role) {
+
+    /**
+     * Roles a player can have
+     */
     public enum Role {
-        ADMIN, BUILD_JUDGE, BUILD_SIZER, PLAYER
+
+        /**
+         * Server Administrator/Operator
+         */
+        ADMIN,
+
+        /**
+         * Judges Builds but is not an administrator/operator
+         */
+        BUILD_JUDGE,
+
+        /**
+         * Has permissions to use WorldEdit to count build sizes
+         */
+        BUILD_SIZER,
+
+        /**
+         * A player with no additional permissions
+         */
+        PLAYER
     }
 }
