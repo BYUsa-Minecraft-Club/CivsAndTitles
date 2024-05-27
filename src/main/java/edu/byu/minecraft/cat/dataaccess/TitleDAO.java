30440f6d416e773c297b3ebed6b8283e1244a506
Michael Davenport <mdaven19@byu.edu>
2024-05-27 16:19:54 -0600

    add base for hidden titles

--- src/main/java/edu/byu/minecraft/cat/dataaccess/TitleDAO.java
+++ src/main/java/edu/byu/minecraft/cat/dataaccess/TitleDAO.java
@@ -2,9 +2,18 @@ package edu.byu.minecraft.cat.dataaccess;
 
 import edu.byu.minecraft.cat.model.Title;
 
+import java.util.Collection;
+
 /**
  * Data Access interface for titles
  */
 public interface TitleDAO extends SimpleDAO<Title, String> {
+
+    /**
+     * Gets all titles visible to players
+     * @return all titles visible to players
+     * @throws DataAccessException If database cannot be accessed
+     */
+    Collection<Title> getVisible() throws DataAccessException;
 
 }
--- src/main/java/edu/byu/minecraft/cat/model/Title.java
+++ src/main/java/edu/byu/minecraft/cat/model/Title.java
@@ -6,5 +6,6 @@ package edu.byu.minecraft.cat.model;
  * @param title       title text
  * @param color       title display color
  * @param description title description, what was required to acquire the title, possible lore
+ * @param hidden      if the title should be displayed as achievable
  */
-public record Title(String title, String color, String description) {}
+public record Title(String title, String color, String description, boolean hidden) {}
