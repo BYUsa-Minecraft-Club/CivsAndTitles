c779c075ace23c95cf56c3c6eb69eaef605ad066
Michael Davenport <mdaven19@byu.edu>
2024-05-27 16:17:08 -0600

    add title documentation

--- src/main/java/edu/byu/minecraft/cat/model/Title.java
+++ src/main/java/edu/byu/minecraft/cat/model/Title.java
@@ -1,3 +1,10 @@
 package edu.byu.minecraft.cat.model;
 
+/**
+ * Values of possible player titles
+ *
+ * @param title       title text
+ * @param color       title display color
+ * @param description title description, what was required to acquire the title, possible lore
+ */
 public record Title(String title, String color, String description) {}
--- src/main/java/edu/byu/minecraft/cat/model/UnlockedTitle.java
+++ src/main/java/edu/byu/minecraft/cat/model/UnlockedTitle.java
@@ -2,4 +2,11 @@ package edu.byu.minecraft.cat.model;
 
 import java.util.UUID;
 
+/**
+ * Represents a title a player has unlocked
+ *
+ * @param uuid   UUID of earning player
+ * @param title  title text
+ * @param earned when the title was earned
+ */
 public record UnlockedTitle(UUID uuid, String title, String earned) {}
