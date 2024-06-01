2aef9aafcb1fd5b76cb75be4b2ed0e46bf2843a2
Michael Davenport <mdaven19@byu.edu>
2024-06-01 16:10:27 -0600

    add civ request dao get for player

--- src/main/java/edu/byu/minecraft/cat/dataaccess/CivRequestDAO.java
+++ src/main/java/edu/byu/minecraft/cat/dataaccess/CivRequestDAO.java
@@ -2,9 +2,19 @@ package edu.byu.minecraft.cat.dataaccess;
 
 import edu.byu.minecraft.cat.model.CivRequest;
 
+import java.util.Collection;
+import java.util.UUID;
+
 /**
  * Data Access interface for civ creation requests
  */
 public interface CivRequestDAO extends SimpleDAO<CivRequest, Integer> {
 
+    /**
+     * Gets all civ requests a player has made
+     * @param uuid uuid of requesting player
+     * @return all active civ requests made by player
+     * @throws DataAccessException If database cannot be accessed
+     */
+    Collection<CivRequest> getForPlayer(UUID uuid) throws DataAccessException;
 }
