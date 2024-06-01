c03c7e3ecb64cac70f835daff7fa96e86e5e1e11
garrett smith <mrgarrettmichaelsmith@gmail.com>
2024-06-01 15:44:30 -0600

    add title commands and PermissionCheckers

--- src/main/java/edu/byu/minecraft/cat/commands/SuggestionProviders.java
+++ src/main/java/edu/byu/minecraft/cat/commands/SuggestionProviders.java
@@ -3,9 +3,16 @@ package edu.byu.minecraft.cat.commands;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import edu.byu.minecraft.cat.CivsAndTitles;
+import edu.byu.minecraft.cat.dataaccess.DataAccessException;
+import edu.byu.minecraft.cat.model.Build;
+import edu.byu.minecraft.cat.model.Civ;
+import edu.byu.minecraft.cat.model.Title;
+import edu.byu.minecraft.cat.model.UnlockedTitle;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 
+import java.util.Collection;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -23,36 +30,88 @@ public class SuggestionProviders {
     }
 
     public static CompletableFuture<Suggestions> allCivs(CommandContext<ServerCommandSource> ctx, SuggestionsBuilder builder) {
-        //TODO
+        try {
+            Collection<Civ> civs = CivsAndTitles.getDataAccess().getCivDAO().getAll();
+            for(Civ civ: civs)
+            {
+                builder.suggest(civ.name());
+            }
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         return builder.buildFuture();
     }
 
     public static CompletableFuture<Suggestions> allBuilds(CommandContext<ServerCommandSource> ctx, SuggestionsBuilder builder) {
-        //TODO
+        try {
+            Collection<Build> builds = CivsAndTitles.getDataAccess().getBuildDAO().getAll();
+            for(Build build: builds)
+            {
+                builder.suggest(build.ID());
+            }
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         return builder.buildFuture();
     }
 
     public static CompletableFuture<Suggestions> myCivs(CommandContext<ServerCommandSource> ctx, SuggestionsBuilder builder) {
         ServerPlayerEntity player = ctx.getSource().getPlayer();
-        //TODO
+        try {
+            Collection<Civ> civs = CivsAndTitles.getDataAccess().getCivDAO().getForPlayer(player.getUuid());
+            for(Civ civ: civs)
+            {
+                builder.suggest(civ.name());
+            }
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         return builder.buildFuture();
     }
 
     public static CompletableFuture<Suggestions> ownedCivs(CommandContext<ServerCommandSource> ctx, SuggestionsBuilder builder) {
         ServerPlayerEntity player = ctx.getSource().getPlayer();
-        //TODO
+        try {
+            Collection<Civ> civs = CivsAndTitles.getDataAccess().getCivDAO().getForPlayer(player.getUuid());
+            for(Civ civ: civs) {
+                if(civ.owner().equals(player.getUuid())){
+                    builder.suggest(civ.name());
+                }
+            }
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         return builder.buildFuture();
     }
 
     public static CompletableFuture<Suggestions> myBuilds(CommandContext<ServerCommandSource> ctx, SuggestionsBuilder builder) {
         ServerPlayerEntity player = ctx.getSource().getPlayer();
-        //TODO
+        try {
+            Collection<Build> builds = CivsAndTitles.getDataAccess().getBuildDAO().getAllForBuilder(player.getUuid());
+            for(Build build: builds)
+            {
+                builder.suggest(build.ID());
+            }
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         return builder.buildFuture();
     }
 
     public static CompletableFuture<Suggestions> myBuildRequests(CommandContext<ServerCommandSource> ctx, SuggestionsBuilder builder) {
         ServerPlayerEntity player = ctx.getSource().getPlayer();
-        //TODO
+        try {
+            Collection<Build> builds = CivsAndTitles.getDataAccess().getBuildDAO().getAllForSubmitter(player.getUuid());
+            for(Build build: builds)
+            {
+                if(build.status() != Build.JudgeStatus.JUDGED)
+                {
+                    builder.suggest(build.ID());
+                }
+            }
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         return builder.buildFuture();
     }
 
@@ -77,13 +136,29 @@ public class SuggestionProviders {
 
     public static CompletableFuture<Suggestions> myTitles(CommandContext<ServerCommandSource> ctx, SuggestionsBuilder builder) {
         ServerPlayerEntity player = ctx.getSource().getPlayer();
-        //TODO
+        try {
+            Collection<UnlockedTitle> titles = CivsAndTitles.getDataAccess().getUnlockedTitleDAO().getAll(player.getUuid());
+            for(UnlockedTitle title: titles)
+            {
+                builder.suggest(title.title());
+            }
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         return builder.buildFuture();
     }
 
     public static CompletableFuture<Suggestions> allTitles(CommandContext<ServerCommandSource> ctx, SuggestionsBuilder builder) {
         ServerPlayerEntity player = ctx.getSource().getPlayer();
-        //TODO
+        try {
+            Collection<Title> titles = CivsAndTitles.getDataAccess().getTitleDAO().getAll();
+            for(Title title: titles)
+            {
+                builder.suggest(title.title());
+            }
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         return builder.buildFuture();
     }
 
--- src/main/java/edu/byu/minecraft/cat/commands/TitleCommands.java
+++ src/main/java/edu/byu/minecraft/cat/commands/TitleCommands.java
@@ -4,10 +4,19 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import edu.byu.minecraft.cat.CivsAndTitles;
+import edu.byu.minecraft.cat.dataaccess.DataAccessException;
+import edu.byu.minecraft.cat.dataaccess.PlayerDAO;
+import edu.byu.minecraft.cat.model.Player;
+import edu.byu.minecraft.cat.model.Title;
+import edu.byu.minecraft.cat.model.UnlockedTitle;
 import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.entity.Entity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
+
+import java.util.Collection;
 
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
@@ -18,27 +27,61 @@ public class TitleCommands {
                 .then(literal("list").executes(TitleCommands::listTitles))
                 .then(literal("change")
                         .then(argument("title", StringArgumentType.string()).suggests(SuggestionProviders::myTitles).requires(PermissionCheckers::hasTitle).executes(TitleCommands::changeTitle)))
-                .then(literal("showRank")
-                        .then(argument("showRank", BoolArgumentType.bool()).executes(TitleCommands::showRank)))
+//                .then(literal("showRank")
+//                        .then(argument("showRank", BoolArgumentType.bool()).executes(TitleCommands::showRank)))
         );
     }
 
     public static Integer listTitles(CommandContext<ServerCommandSource> ctx) {
         ctx.getSource().sendFeedback(()-> Text.literal("List titles"), false);
+        try {
+           Collection<Title> titles = CivsAndTitles.getDataAccess().getTitleDAO().getVisible();
+           for(Title title: titles)
+           {
+                ctx.getSource().sendFeedback(()-> Text.literal(title.title() + "  -  " + title.description()), false);
+           }
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         return 1;
     }
 
     public static Integer changeTitle(CommandContext<ServerCommandSource> ctx) {
         String title = ctx.getArgument("title", String.class);
         ctx.getSource().sendFeedback(()->Text.literal("Change title to " + title), false);
+
+        Entity entity = ctx.getSource().getEntity();
+        if (entity == null)
+        {
+            return 0;
+        }
+
+        try {
+            PlayerDAO playerDAO = CivsAndTitles.getDataAccess().getPlayerDAO();
+            Player player = playerDAO.get(entity.getUuidAsString());
+            Collection<UnlockedTitle> validTitle = CivsAndTitles.getDataAccess().getUnlockedTitleDAO().getAll(entity.getUuidAsString());
+            if(validTitle.stream().anyMatch(ut->ut.title().equals(title)))
+            {
+                player = player.setTitle(title);
+                playerDAO.update(player);
+            }
+            else
+            {
+                ctx.getSource().sendFeedback(()->Text.literal("Not an unlocked title: " + title), false);
+                return 0;
+            }
+
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         return 1;
     }
 
-    public static Integer showRank(CommandContext<ServerCommandSource> ctx) {
-        Boolean showRank = ctx.getArgument("showRank", Boolean.class);
-        ctx.getSource().sendFeedback(()->Text.literal("Change show rank to " + showRank), false);
-        return 1;
-    }
+//    public static Integer showRank(CommandContext<ServerCommandSource> ctx) {
+//        Boolean showRank = ctx.getArgument("showRank", Boolean.class);
+//        ctx.getSource().sendFeedback(()->Text.literal("Change show rank to " + showRank), false);
+//        return 1;
+//    }
 
 
 
