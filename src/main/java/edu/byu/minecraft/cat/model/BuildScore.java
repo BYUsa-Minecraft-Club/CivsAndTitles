91440a2ae903dc1c4ba73f1e97af0456b10863ac
Michael Davenport <mdaven19@byu.edu>
2024-06-06 14:48:45 -0600

    fix build score add build id

--- src/main/java/edu/byu/minecraft/cat/model/BuildScore.java
+++ src/main/java/edu/byu/minecraft/cat/model/BuildScore.java
@@ -6,6 +6,7 @@ import java.util.UUID;
  * Represents the scores from one judge on one build
  *
  * @param ID            unique ID
+ * @param buildID       ID of build this score is associated with
  * @param judge         UUID of build judge player
  * @param functionality functionality score zero to ten
  * @param technical     technical score zero to five
@@ -20,6 +21,6 @@ import java.util.UUID;
  * @param total         total score zero to fifty if not in a civ, or fifty-five if in a civ
  * @param comments      any comments from the build judge
  */
-public record BuildScore(int ID, UUID judge, int functionality, int technical, int texture, int storytelling,
-                         Integer thematic, int landscaping, int detailing, int lighting, int layout, int discretion,
-                         int total, String comments) {}
+public record BuildScore(int ID, int buildID, UUID judge, int functionality, int technical, int texture,
+                         int storytelling, Integer thematic, int landscaping, int detailing, int lighting, int layout,
+                         int discretion, int total, String comments) {}
