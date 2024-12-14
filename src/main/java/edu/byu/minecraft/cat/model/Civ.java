f08990579bf67aed21af9e02d1325927db221f8b
Michael Davenport <mdaven19@byu.edu>
2024-12-14 12:37:15 -0700

    fix civ model

--- src/main/java/edu/byu/minecraft/cat/model/Civ.java
+++ src/main/java/edu/byu/minecraft/cat/model/Civ.java
@@ -11,16 +11,11 @@ import java.util.UUID;
  * @param numPoints    total build points this civ has earned
  * @param incorporated if this civ is incorporated (has a border, approved by an admin)
  * @param active       if this civ is currently marked as active
- * @param founder      UUID of founding player
- * @param owner        UUID of current owner player
- * @param leaders      set of UUIDs of civ leader players
- * @param contributors set of UUIDs of players who have contributed to civ builds
- * @param members      set of UUIDS of civ member players
- * @param location     location of civ
+ * @param locationID   ID of location of civ
  * @param createdDate  date/time of civ creation
  */
-public record Civ(int ID, String name, int numPoints, boolean incorporated, boolean active, UUID founder, UUID owner,
-                  Set<UUID> leaders, Set<UUID> contributors, Set<UUID> members, Location location, String createdDate) {
+public record Civ(int ID, String name, int numPoints, boolean incorporated, boolean active,
+                  int locationID, String createdDate) {
 
     /**
      * Adds points to a civ
@@ -28,7 +23,6 @@ public record Civ(int ID, String name, int numPoints, boolean incorporated, bool
      * @return a new Civ object with number of points added
      */
     public Civ addPoints(int points) {
-        return new Civ(ID, name, numPoints + points, incorporated, active, founder, owner, leaders, contributors,
-                members, location, createdDate);
+        return new Civ(ID, name, numPoints + points, incorporated, active, locationID, createdDate);
     }
 }
